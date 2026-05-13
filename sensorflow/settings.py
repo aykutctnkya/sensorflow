edd9923a18db966a5fcd042ca18992c04b40fbab
aykutctnkya <aykut.cetinkaya@yahoo.com>
2026-05-13 23:06:01 +0200

    Fix Redis host configuration for CI

--- sensorflow/settings.py
+++ sensorflow/settings.py
@@ -154,7 +154,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('redis', 6379)],
+            'hosts': [(os.environ.get('REDIS_HOST', 'redis'), 6379)],
         },
     },
 }
